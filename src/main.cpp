62c2e19b4f532042fbf32b7fde26b84d9988ced8
lunarseer <igor.chopenko@gmail.com>
2023-06-05 07:03:51 +0300

    sync

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,28 @@
 #include <Arduino.h>
+#include <NanitLib.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define MIC_APIN P4_1
+#define MIC_DPIN P4_2
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  Nanit_Base_Start();
+  pinMode(MIC_APIN, INPUT);
+  pinMode(MIC_DPIN, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  int dval = digitalRead(MIC_DPIN);
+  int aval = analogRead(MIC_APIN);
+  Serial.print("Analog: ");
+  Serial.print(aval);
+  Serial.print(" ");
+  if (aval > 520) {
+    GET_NANIT.BuildinRGB(255, 0, 0);
+    Serial.println("Sound detected"); 
+    delay(1000);
+    GET_NANIT.BuildinRGB(0, 0, 0);
+  } else {
+    Serial.println("No sound");  
+  }
 }
