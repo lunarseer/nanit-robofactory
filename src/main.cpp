e0dab5aa2ed9c49cace8ac96e8adab2c87717f07
lunarseer <igor.chopenko@gmail.com>
2023-06-11 20:16:35 +0300

    sync

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,61 @@
 #include <Arduino.h>
+#include <NanitLib.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define PIR_PIN P5_1
+
+
+int counter = 0;
+
+bool motion_flag = false;
+bool status_red_led = false;
+bool status_green_led = false;
+int red_blink_count = 20;
+
+unsigned long red_blink_last_time = 0;
+unsigned long red_blink_interval = 200;
+
+unsigned long green_blink_last_time = 0;
+unsigned long green_blink_interval = 1000; // green blink interval
+
+void detectMotion() {
+  motion_flag = true;
+  counter++;
+  Serial.print("Motion Detected: ");
+  Serial.println(counter);
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  Nanit_Base_Start();
+  pinMode(PIR_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(PIR_PIN), detectMotion, RISING);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (motion_flag){
+    if (millis() - red_blink_last_time > red_blink_interval){
+      red_blink_last_time = millis();
+      status_red_led = !status_red_led;
+      if(status_red_led) {
+        GET_NANIT.BuildinRGB(255, 0, 0);
+      } else {
+        GET_NANIT.BuildinRGB(0, 255, 0);
+      }
+      red_blink_count--;
+      if (red_blink_count == 0){
+        motion_flag = false;
+        red_blink_count = 20;
+      }
+    }
+  } else {
+    if (millis() - green_blink_last_time > green_blink_interval){
+      green_blink_last_time = millis();
+      status_green_led = !status_green_led;
+      if (status_green_led){ 
+        GET_NANIT.BuildinRGB(0, 255, 0); 
+      }else{ 
+        GET_NANIT.BuildinRGB(0, 0, 0); 
+      } 
+    }
+  }
 }
