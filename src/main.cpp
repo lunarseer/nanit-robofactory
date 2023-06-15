7a3450963087c19f33bd0a19ae3faae030f0e7fc
lunarseer <igor.chopenko@gmail.com>
2023-06-15 20:37:08 +0300

    CO2 read

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
 #include <Arduino.h>
+#include <NanitLib.h>
 
-// put function declarations here:
-int myFunction(int, int);
+
+#define CO2_PIN_A P4_1
+#define CO2_PIN_D P4_2
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  Nanit_Base_Start();
+  tft.fillScreen(ST7735_BLACK);
+  tft.setTextColor(ST7735_WHITE, ST7735_BLACK);
+  GET_NANIT.BuildinRGB(0, 0, 0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // int co2_d = digitalRead(CO2_PIN_D);
+  int co2_a = analogRead(CO2_PIN_A);
+  Serial.println(co2_a);
+  tft.setCursor(30, 50);
+  tft.print("CO2: ");
+  tft.println(co2_a);
+  delay(250);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+// vcc +
+// gnd -
+// D P4_2
+// A P4_1
